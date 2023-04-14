121547949475d297ead7355a29c0ffa3786125a4
Ronny Granados <ronnygranados7@gmail.com>
2023-04-14 15:51:28 -0600

    adder module

--- adder.v
+++ adder.v
@@ -1,5 +1,17 @@
 module adder(a, b, cin, q, cout);
 
-    // Comments
+    // Module inputs
+    input a, b, cin;
+
+    // Module outputs
+    output q, cout;
+
+    // Module operations
+
+    /* SUM operation */
+    assign sum = a ^ b ^ cin;
+
+    /* CARRY-OUT operation */
+    assign cout = (a & b) | (cin & a) | (cin & b);
 
 endmodule
